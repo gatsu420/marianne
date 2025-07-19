540e21f5bfecfff59a171c3826ac898b367c0839
Ranggalawe Istifajar <51109081+gatsu420@users.noreply.github.com>
2025-07-19 16:21:20 +0700

    add helper for testing

--- common/tests/assert.go
+++ common/tests/assert.go
@@ -1,11 +1,11 @@
-package testassert
+package tests
 
 import (
 	"reflect"
 	"testing"
 )
 
-func Equal(t *testing.T, actual interface{}, expected interface{}) {
+func AssertEqual(t *testing.T, actual interface{}, expected interface{}) {
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("had %v, expected %v", actual, expected)
 	}
